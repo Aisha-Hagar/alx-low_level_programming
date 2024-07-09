2802eeab4f6e613c7eeebe6ec43122b9011b8f38
A-H <aisha.hhagar@gmail.com>
2024-07-09 13:23:10 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,7 +22,7 @@ void print_to_98(int n)
 				printf(", ");
 			}
 		}
-	}	
+	}
 	else
 	{
 		for (i = n; i <= 98; i++)
@@ -34,5 +34,5 @@ void print_to_98(int n)
 			}
 		}
 	}
-	printf("\n");	
+	printf("\n");
 }
